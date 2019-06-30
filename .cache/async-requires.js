6ee149ba30764fd86423b5baacf88a4af0a2189c
Nevers <livelyevers@gmail.com>
2019-06-29 21:35:52 -0700

    Run build

---
+++ .cache/async-requires.js
@@ -0,0 +1,9 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-pages-404-js": () => import("/Users/nickyevers/Desktop/WEB/PERSONAL-PROJECTS/steady-hand-tea/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/Users/nickyevers/Desktop/WEB/PERSONAL-PROJECTS/steady-hand-tea/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-pricelist-js": () => import("/Users/nickyevers/Desktop/WEB/PERSONAL-PROJECTS/steady-hand-tea/src/pages/Pricelist.js" /* webpackChunkName: "component---src-pages-pricelist-js" */)
+}
+
