85c5de10ef8cc4c0e7496ddeeb62dd77ceb7e9ef
Nevers <livelyevers@gmail.com>
2019-11-27 00:46:24 -0800

    Run build

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,10 +2,8 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-index-js": () => import("../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-pricelist-js": () => import("../src/pages/pricelist.js" /* webpackChunkName: "component---src-pages-pricelist-js" */),
-  "component---src-pages-pricelist-3-js": () => import("../src/pages/pricelist3.js" /* webpackChunkName: "component---src-pages-pricelist-3-js" */)
+  "component---src-pages-pricelist-js": () => import("../src/pages/pricelist.js" /* webpackChunkName: "component---src-pages-pricelist-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,10 +5,8 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/nickyevers/Desktop/WEB/PERSONAL-PROJECTS/steady-hand-tea/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/nickyevers/Desktop/WEB/PERSONAL-PROJECTS/steady-hand-tea/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/nickyevers/Desktop/WEB/PERSONAL-PROJECTS/steady-hand-tea/src/pages/index.js"))),
-  "component---src-pages-pricelist-js": hot(preferDefault(require("/Users/nickyevers/Desktop/WEB/PERSONAL-PROJECTS/steady-hand-tea/src/pages/pricelist.js"))),
-  "component---src-pages-pricelist-3-js": hot(preferDefault(require("/Users/nickyevers/Desktop/WEB/PERSONAL-PROJECTS/steady-hand-tea/src/pages/pricelist3.js")))
+  "component---src-pages-pricelist-js": hot(preferDefault(require("/Users/nickyevers/Desktop/WEB/PERSONAL-PROJECTS/steady-hand-tea/src/pages/pricelist.js")))
 }
 
