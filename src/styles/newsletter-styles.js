7cfdccca036a92f649afa1c9f43390c9e3ceb9eb
Nevers <livelyevers@gmail.com>
2019-11-29 20:14:18 -0800

    Update mobile image again

--- src/styles/newsletter-styles.js
+++ src/styles/newsletter-styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Wrapper, SubTitle } from '../components/Helpers';
 
 const NewsletterWrapper = styled(Wrapper)`
-  background-image: url('https://res.cloudinary.com/nicky-cloudinary/image/upload/s--lScOneY_--/f_auto,q_jpegmini:1/v1575086423/steady-hand-tea/TAIWAN-MISC-52_copy_2.jpg');
+  background-image: url('https://res.cloudinary.com/nicky-cloudinary/image/upload/s--HjnDN58---/f_auto,q_jpegmini:1/v1575087205/steady-hand-tea/TAIWAN-MISC-52_copy_2.jpg');
   background-size: cover;
   background-attachment: fixed;
   background-position: center center;
