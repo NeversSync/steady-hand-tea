fd812e1decd9af357b5ec08cb7cada87ce5179ca
Nevers <livelyevers@gmail.com>
2020-04-23 12:25:03 -0700

    Update pricelist

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"UA-134726933-2","head":true},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
